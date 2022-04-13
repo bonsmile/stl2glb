b7686ddac84681732b7195ea1879b5cf4cd74026
馬迪 <di.ma@bonsmile.com>
2022-04-13 14:47:19 +0800

    impl: 通过对话框选择 stl 文件

---
+++ src/MainWindow.cpp
@@ -0,0 +1,20 @@
+﻿#include "MainWindow.h"
+#include "imgui/imgui.h"
+#include "portable-file-dialogs.h"
+#include "lxd/debug.h"
+
+void MainWindow() {
+	ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
+	ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+	ImGui::Begin("stl2glb", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
+	{
+		if(ImGui::Button("选择 STL 文件")) {
+			auto dialog = pfd::open_file("Select file to open", "", {"All files", "*.stl"}, true);
+
+			for(auto& file : dialog.result()) {
+				lxd::print("select file:{}\n", file);
+			}
+		}
+	}
+	ImGui::End();
+}
